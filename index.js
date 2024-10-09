1e69f8b4cf62869834e33feaef3a1bfe7043aa3d
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-10-09 13:38:17 +1300

    test memory

---
+++ index.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+
+const master = []
+
+for (let i = 0; i < 30; i++) {
+    master.push(crypto.randomBytes(2**31 - 1))
+}
+
+console.log('done')
+setTimeout(() => {}, 10000000)
